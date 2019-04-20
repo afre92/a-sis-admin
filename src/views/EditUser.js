17f3c78f9ee6480c4f88c3557f749d1f40994e59
Andres <afre92@gmail.com>
2019-04-20 14:53:58 -0400

    edit user view cleaned up

--- src/views/EditUser.js
+++ src/views/EditUser.js
@@ -8,67 +8,43 @@ import {
   ListGroupItem,
   Row,
   Form,
-  FormGroup,
   FormInput,
   Col,
   Card,
-  CardBody,
-  CardFooter,
-  Badge,
   Button,
-  FormSelect,
-  FormTextarea,
 } from "shards-react";
 
 import CustomFileUpload from "../components/components-overview/CustomFileUpload";
 import PageTitle from "../components/common/PageTitle";
-import Dropzone from 'react-dropzone';
 
-const baseStyle = {
-  flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '40px',
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  outline: 'none',
-  transition: 'border .24s ease-in-out'
+const styles = {
+  mainContainer:  {
+    paddingLeft: '7%',
+    paddingRight: '7%'
+  }
 };
 
 class EditUser extends React.Component {
 
- 
   constructor(props) {
     super(props);
 
     this.state = {
-      // First list of posts.
     };
   }
-
-  onDrop(imageFiles) {
-    this.setState({
-        imageFiles: imageFiles
-    })
-    console.log(imageFiles)  
-}
 
   render() {
 
     return (
-      <Container className="main-content-container" style={{paddingLeft: '7%', paddingRight: '7%', height: '100%'}}>
-        {/* Page Header */}
+      <Container fluid className="main-content-container">
+        <div style={styles.mainContainer}>
+
         <Row noGutters className="page-header py-4">
           <PageTitle sm="4" title="Edit User" subtitle="Users" className="text-sm-left" />
         </Row>
 
-        {/* First Row of Posts */}
-        <div style={{textAlign: 'center'}}>
+
+        <div className="text-center">
           <Col lg="6" style={{display: 'inline-block'}}>
             <Card small className="mb-4">
               <CardHeader className="border-bottom">
@@ -78,57 +54,49 @@ class EditUser extends React.Component {
                 <ListGroupItem className="p-3">
                   <Row>
                     <Col>
-                      <Form style={{textAlign: 'left'}}>
+                      <Form className="text-left">
                         <Row form>
-                          {/* First Name */}
                           <Col md="12" className="form-group">
-                            <label htmlFor="feFirstName">First Name</label>
+                            <label htmlFor="FirstName">First Name</label>
                             <FormInput
                               value="Sierra"
-                              id="feFirstName"
+                              id="FirstName"
                               onChange={() => {}}
                             />
                           </Col>
-                          {/* Last Name */}
                         </Row>
                         <Row form>
-                          {/* First Name */}
                           <Col md="12" className="form-group">
-                            <label htmlFor="feFirstName">Last Name</label>
+                            <label htmlFor="lastName">Last Name</label>
                             <FormInput
                               value="Brooks"
-                              id="feFirstName"
+                              id="lastName"
                               onChange={() => {}}
                             />
                           </Col>
-                          {/* Last Name */}
                         </Row>
                         <Row form>
-                          {/* First Name */}
                           <Col md="12" className="form-group">
-                            <label htmlFor="feFirstName">Email</label>
+                            <label htmlFor="email">Email</label>
                             <FormInput
                               value="sierra@email.com"
-                              id="feFirstName"
+                              id="email"
                               onChange={() => {}}
                             />
                           </Col>
-                          {/* Last Name */}
                         </Row>
                         <Row form>
-                          {/* First Name */}
                           <Col md="12" className="form-group">
-                            <label htmlFor="feFirstName">Password</label>
+                            <label htmlFor="password">Password</label>
                             <FormInput
-                              id="feFirstName"
+                              id="password"
                               onChange={() => {}}
                             />
                           </Col>
-                          {/* Last Name */}
                         </Row>
                         <Row form>
                           <Col md="12" className="form-group">
-                           <label htmlFor="feFirstName">Image</label>
+                           <label htmlFor="image">Image</label>
                             <CustomFileUpload />
                           </Col>
                         </Row>
@@ -143,7 +111,7 @@ class EditUser extends React.Component {
           </Col>
         </div>
 
-
+        </div>
       </Container>
     );
   }
